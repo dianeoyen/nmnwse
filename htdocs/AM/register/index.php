d9dd9bdda3a07811ce05fe25655ce1dd7f980feb
dianeoyen <doyen@cs.unm.edu>
2016-09-02 09:04:51 -0600

    Update AM 2016 registration.

---
+++ htdocs/AM/register/index.php
@@ -0,0 +1,35 @@
+<?php
+   header("Location: https://goo.gl/forms/tnLtb5o9YSG5hBA53");
+?>
+<html>
+<head>
+<title>NMNWSE Annual Symposium Registration</title>
+</head/>
+<?php
+include("../../Includes/pagetop1_nmnwse.php");
+?>
+NMNWSE Annual Symposium Registration
+<?php
+include("../../Includes/pagetop2_nmnwse.php");
+?>
+<P>&nbsp; <br>
+
+
+<P>
+<img src="http://nmnwse.org/Images/button.gif">
+    <font color="#cb6940" size="+1"><b>NMNWSE Annual Symposium Registration</b></font>
+</P>
+
+Registration is now open! You are being redirected to:
+<P>
+<font size=+1 font="Arial,Helvetica"><B><a href="https://goo.gl/forms/tnLtb5o9YSG5hBA53">https://goo.gl/forms/tnLtb5o9YSG5hBA53</a></B></font>
+
+<! ------------------------------------------ >
+
+<P>&nbsp;
+<?php
+include("../../Includes/pagebot_nmnwse.php");
+?>
+
+<! ------------------------------------------ >
+
