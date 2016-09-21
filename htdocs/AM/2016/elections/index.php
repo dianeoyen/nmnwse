1ba7df0d1726e85fa6113b7975d05874a72065e2
Diane Oyen <doyen@lanl.gov>
2016-09-21 16:25:36 -0600

    Added 2016 election stuff.

---
+++ htdocs/AM/2016/elections/index.php
@@ -0,0 +1,280 @@
+<html>
+<head>
+<title>2016 NMNWSE Elections for Oct 2016-Oct 2017 Board - Online Voting Form</title>
+<?php
+include("../../../Includes/pagetop1_nmnwse.php");
+?>
+2016 NMNWSE ELECTIONS FOR Oct 2016 - Oct 2017 BOARD
+<?php
+include("../../../Includes/pagetop2_nmnwse.php");
+?>
+
+<! --------------- >
+
+<form method="post" action="/cgi-bin/formto.pl">
+ 
+<P><img src="http://nmnwse.org/Images/r_hand.gif">&nbsp;<a href="notes.php">Click here for voting instructions and notes re alternate methods to vote, etc.</a>
+<br>Online voting opens 8am MST Monday, 17 Oct 2016 and closes 8pm MST on Thursday, 20 Oct 2016. In person voting takes place Saturday, 22 Oct 2016, at the Annual Business Meeting in Albuquerque.
+
+
+<P><img src="http://nmnwse.org/Images/r_hand.gif">&nbsp;
+<a href="profiles.php">Click here for the candidate profiles.</a>
+<!--
+<P><img src="http://nmnwse.org/Images/bl_pin.gif"><b>Breaking News</b>: DATE<br>
+We currently have NUMBER new candidates&nbsp;<img src="http://nmnwse.org/Images/updated.gif">
+<br><img src="http://nmnwse.org/Images/button.gif" height=10>&nbsp;POSITION candidate NAME DATE.
+-->
+
+<center>
+<br>
+<font size="+3" color="red"> <! #33CC00 ><B>
+       <img src="http://nmnwse.org/Images/button.gif" height="12">
+<!--
+       ONLINE VOTING CLOSED
+-->
+       DRAFT - VOTING NOT YET OPEN
+<!--
+       <input type="submit" name="Submit1" value=" SUBMIT VOTE">
+-->
+       <img src="http://nmnwse.org/Images/button.gif" height="12">
+    </B> </font>
+<!--
+<br> You can still vote in person at the annual meeting,
+or call in your vote to Tinka at 505-690-1318 by 9am Saturday.
+-->
+</center>
+<center>
+<IMG SRC="http://nmnwse.org/Images/linefrill.gif" ALT="-----------" HEIGHT=5 WIDTH=30%>
+</center>
+
+
+<P>
+<b><sup>*</sup>This form is for:</b> <input type="text" name="Person" size="50">
+(enter your name - see <a href="notes.php">instructions</a>)
+
+<P>
+<B><sup>*</sup>Are you currently a member in good standing, or will be by 22 Oct 2016?</B>
+<blockquote>
+<input type="radio" name="Member" value="Yes">Yes&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" name="Member" value="No">No&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="radio" name="Member" value="Unsure">Unsure
+</blockquote>
+
+<P>
+<B>Your Votes:</B>
+<br>You may vote for a given person only once, and must select a choice for each declared candidate (including not voting for them). You may vote for only 1 person for each of President, Vice-President, and Secretary, and up to 5 people for At-Large. Write-in's, as well as volunteers to be "late additions to slate", are very welcome!
+<P>
+<table border=1 cellpadding=5>
+<tr><td colspan=7 valign="center" align="center" bgcolor="#ccccff">Board Positions</td><tr>
+<tr>
+<td valign="middle" align="center" rowspan="2">Name</td>
+<td valign="middle" align="center" rowspan="2">Chapter</td>
+<td colspan=4 valign="middle" align="center"> Position</td>
+<td valign="middle" align="center" rowspan="2">not voting<br> for this<br> candidate</td>
+</tr><tr>
+<td valign="middle" align="center" bgcolor="#ccffcc">Pres<br>(Vote for 1)</td>
+<td valign="middle" align="center" bgcolor="#ccffff">VP/P-elect<br>(Vote for 1)</td>
+<td valign="middle" align="center" bgcolor="#ffffcc">Treas<br>(Vote for 1)</td>
+<td valign="middle" align="center" bgcolor="#ffccff">At-Large<br>(Vote for<br>&nbsp;up to 5)</td>
+</tr>
+<tr>
+<td valign="top"><sup>*</sup>Rebecca Galves</td>
+<td valign="top">Southern</td>
+<td valign="top" align="center" bgcolor="#ccffcc"><input type="radio" name="Galves" value="Pres"> </td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center"><input type="radio" name="Galves" value="No"> </td>
+</tr>
+<!--
+<tr>
+<td valign="top"><sup>*</sup></td>
+<td valign="top">Central</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center" bgcolor="#ccffff"><input type="radio" name="Vance" value="VP"> </td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center"><input type="radio" name="Vance" value="No"> </td>
+</tr>
+-->
+<tr>
+<td valign="top"><sup>*</sup>Jeanne Banks</td>
+<td valign="top">Central</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center" bgcolor="#ffffcc"><input type="radio" name="Banks" value="Treas"> </td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center"><input type="radio" name="Banks" value="No"> </td>
+</tr>
+<tr>
+<td valign="top"><sup>*</sup>Sarah Balizan</td>
+<td valign="top">Southern</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center" bgcolor="#ffccff"><input type="radio" name="Balizan" value="At-Large"> </td>
+<td valign="top" align="center"><input type="radio" name="Balizan" value="No"> </td>
+</tr>
+<!--
+<tr>
+<td valign="top"><sup>*</sup>Cheri Burch</td>
+<td valign="top">Central</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center" bgcolor="#ffccff"><input type="radio" name="Burch" value="At-Large"> </td>
+<td valign="top" align="center"><input type="radio" name="Burch" value="No"> </td>
+</tr>
+<tr>
+<td valign="top"><sup>*</sup>Dana Roberson</td>
+<td valign="top">Central</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center" bgcolor="#ffccff"><input type="radio" name="Roberson" value="At-Large"> </td>
+<td valign="top" align="center"><input type="radio" name="Roberson" value="No"> </td>
+</tr>
+<tr>
+<td valign="top"><sup>*</sup>Claudia Barreto</td>
+<td valign="top">Central</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center" bgcolor="#ffccff"><input type="radio" name="Barreto" value="At-Large"> </td>
+<td valign="top" align="center"><input type="radio" name="Barreto" value="No"> </td>
+</tr>
+-->
+<tr>
+<td valign="top" colspan=2>Write in No. 1<br>
+<input type="text" name="WriteInName1" size="30">
+</td>
+<td valign="middle" align="center" bgcolor="#ccffcc"><input type="radio" name="WriteInPos1" value="Pres"> </td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<!--
+<td valign="middle" align="center" bgcolor="#ccffff"><input type="radio" name="WriteInPos1" value="VP"> </td>
+<td valign="middle" align="center" bgcolor="#ffffcc"><input type="radio" name="WriteInPos1" value="Sect"> </td>
+-->
+<td valign="middle" align="center" bgcolor="#ffccff"><input type="radio" name="WriteInPos1" value="At-Large"> </td>
+<td valign="middle" align="center">&nbsp; </td>
+</tr>
+<tr>
+<td valign="top" colspan=2>Write in No. 2<br>
+<input type="text" name="WriteInName2" size="30">
+</td>
+<!--
+<td valign="middle" align="center" bgcolor="#ccffcc"><input type="radio" name="WriteInPos2" value="Pres"> </td>
+-->
+<td valign="top" align="center">&nbsp;</td>
+<td valign="middle" align="center" bgcolor="#ccffff"><input type="radio" name="WriteInPos2" value="VP"> </td>
+<td valign="top" align="center">&nbsp;</td>
+<!--
+<td valign="middle" align="center" bgcolor="#ffffcc"><input type="radio" name="WriteInPos2" value="Sect"> </td>
+-->
+<td valign="middle" align="center" bgcolor="#ffccff"><input type="radio" name="WriteInPos2" value="At-Large"> </td>
+<td valign="middle" align="center">&nbsp; </td>
+</tr>
+<tr>
+<td valign="top" colspan=2>Write in No. 3<br>
+<input type="text" name="WriteInName3" size="30">
+</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<!--
+<td valign="middle" align="center" bgcolor="#ccffcc"><input type="radio" name="WriteInPos3" value="Pres"> </td>
+<td valign="middle" align="center" bgcolor="#ccffff"><input type="radio" name="WriteInPos3" value="VP"> </td>
+-->
+<td valign="middle" align="center" bgcolor="#ffffcc"><input type="radio" name="WriteInPos3" value="Treas"> </td>
+<td valign="middle" align="center" bgcolor="#ffccff"><input type="radio" name="WriteInPos3" value="At-Large"> </td>
+<td valign="middle" align="center">&nbsp; </td>
+</tr>
+<tr>
+<td valign="top" colspan=2>Write in No. 4<br>
+<input type="text" name="WriteInName4" size="30">
+</td>
+<! orange: ffcc66, gr: 99ff99, ccffcc lighter >
+<!--
+<td valign="middle" align="center" bgcolor="#ccffcc"><input type="radio" name="WriteInPos4" value="Pres"> </td>
+<td valign="middle" align="center" bgcolor="#ccffff"><input type="radio" name="WriteInPos4" value="VP"> </td>
+<td valign="middle" align="center" bgcolor="#ffffcc"><input type="radio" name="WriteInPos4" value="Sect"> </td>
+-->
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="top" align="center">&nbsp;</td>
+<td valign="middle" align="center" bgcolor="#ffccff"><input type="radio" name="WriteInPos4" value="At-Large"> </td>
+<td valign="middle" align="center">&nbsp; </td>
+</tr>
+</table>
+
+<P>
+<B>Comments</B>, if any: <br>
+<br>
+<textarea name="Comments" rows="2" cols="75" wrap="Virtual"></textarea> 
+<br>
+
+
+<P>
+<!--
+<table border=0 width=100%><tr><td align="left" valign="top">
+<a href="profiles.php" target=_blank>Candidate Profiles</a>
+<img src="http://nmnwse.org/Images/l_hand.gif">
+</td>
+<td align="right" valign="top"> <img src="http://nmnwse.org/Images/r_hand.gif"> <a href="profiles.php" target=_blank>Voting Instructions</a></td></tr></table>
+-->
+
+<IMG SRC="http://nmnwse.org/Images/linefrill.gif" ALT="-----------" HEIGHT=5 WIDTH=100%>
+<!--
+<div align="right">
+</div>
+-->
+<table border=0 width="100%">
+  <tr>
+    <td align="left" valign="top">
+           <table border=2 bgcolor="red"><tr><td>
+              <a href="http://nmnwse.org/AM/2016/elections/">
+                 <font size="-1" color="white"><B>&nbsp;CLEAR&nbsp;</B></font>
+               </a>
+           </td></tr></table>
+    </td>
+    <td align="center"> <font size="+2" color="red"><B>  <! green=#33CC00 >
+       <img src="http://nmnwse.org/Images/button.gif" height="12">
+       DRAFT - VOTING NOT YET OPEN
+       <img src="http://nmnwse.org/Images/button.gif" height="12">
+       </B> </font>
+<!-- <br> see top of page for call-in instructions. -->
+<!--
+       <input type="submit" name="Submit1" value=" SUBMIT VOTE">
+        DRAFT - VOTING NOT YET OPEN
+       <! VOTING CLOSED ><br> submit button removed
+      <br>You may vote in person at the Annual Meeting
+      <br> or by call-in, see <a href="notes.php">notes</a>.
+    <br>VOTING NOT YET OPEN <br> SUBMIT BUTTON FOR TEST PURPOSES ONLY
+       VOTING NOT YET OPEN<br> submit button removed
+-->
+</td>
+    <td align="right" valign="top"> <font size="-1" face="Arial, Helvetica">
+       <sup>*</sup>Required Field<br>
+       All fields are required<br>except write-ins<br>and comments.
+    </font> </td>
+</tr></table>
+
+<!--SCRIPT CONFIGURATION SECTION -->
+<input type="hidden" name="required"
+value="Person,Member,Galves,Vance,Torres,Shindi,Burch,Roberson,Barreto"> 
+<input type="hidden" name="data_order"
+value="Person,Member,Galves,Vance,Torres,Shindi,Burch,Roberson,Barreto,WriteInName1,WriteInPos1,WriteInName2,WriteInPos2,WriteInName3,WriteInPos3,WriteInName4,WriteInPos4,Comments">
+<input type="hidden" name="submit_to" value=" ">  <! insert valid later > 
+<input type="hidden" name="form_id" value="Oct 2016 NMNWSE Board Elections - Online Vote"> 
+<input type="hidden" name="ok_url" value="http://nmnwse.org/AM/2016/elections/vote_thanks.htm"> 
+<input type="hidden" name="not_ok_url" value="http://nmnwse.org/AM/2016/elections/vote_tryagain.htm"> 
+<!--END OF SCRIPT CONFIGURATION SECTION -->
+
+</form>
+
+
+<! --------------- >
+
+
+<?php
+include("../../../Includes/pagebot_nmnwse.php");
+?>
---
+++ htdocs/AM/2016/elections/profiles.php
@@ -0,0 +1,168 @@
+<html>
+<head>
+<title>2016 NMNWSE Elections for Oct 2016 - Oct 2017 Board - Candidate Profiles</title>
+<?php
+include("../../../Includes/pagetop1_nmnwse.php");
+?>
+2016 NMNWSE ELECTIONS FOR Oct 2016 - Oct 2017 Board
+<br>Candidate Profiles
+<?php
+include("../../../Includes/pagetop2_nmnwse.php");
+?>
+
+<! --------------- >
+
+
+<! ================================================== >
+<! top stuff >
+
+<P>
+<!--
+Candidates are listed in alphabetic order by position, and provided their own profile (although we may have edited to shorten). 
+-->
+The NMNWSE Board's only position on the candidates is that we feel all the declared candidates are qualified, and we encourage all members to vote for the declared or write-in candidates of their choice.
+
+<P>
+<img src="http://nmnwse.org/Images/r_hand.gif">&nbsp;<a href="notes.php">Voting Notes and Instructions</a>
+<! br > &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<img src="http://nmnwse.org/Images/r_hand.gif">&nbsp;<a href="index.php">Online Voting Form</a>
+
+<!--
+<P><img src="http://nmnwse.org/Images/bl_pin.gif"><b>Breaking News</b>: We have new candidates&nbsp;<img src="http://nmnwse.org/Images/updated.gif">
+<br><img src="http://nmnwse.org/Images/button.gif" height=10>&nbsp;(position) candidate (name) added (date)
+-->
+
+<!--
+<P>
+<img src="http://nmnwse.org/Images/button.gif">&nbsp;<font size="+1"><B>NEED VOLUNTEER</b></font> 
+<br>We currently have no volunteer to run for this position - please contact the NMNWSE Board if you might be willing.
+<br><img src="http://nmnwse.org/Images/bl_pin.gif"><I> Chapter Membership...</I>
+-->
+
+
+<! ================================================== >
+<! positions >
+
+<! ------------------------------------------------------------ >
+<! Pres as no Vice-Pres... >
+<P>
+<center>
+<hr width="30%">
+<B><FONT color="#cb6940" size="+2">PRESIDENT</b></font>
+</center>
+
+<P>
+<img src="http://nmnwse.org/Images/button.gif">&nbsp;<font size="+1"><B>Rebecca (Becca) Galves</b></font> 
+<br>
+<! 2015 profile >
+Rebecca Galves is a motivated leader in outreach and broadening the participation of women and underrepresented minorities in computing, science, technology, engineering, and mathematics (STEM). With degrees in Information Systems, Geography, and Business, Rebecca is responsible for extensive program development, management, and expansion of initiatives across New Mexico.  
+In her five-year tenure at NMSU, Rebecca and her team of outstanding young women have provided out-of-school computing and STEM programming to over 14,000 K - 12 students throughout the state, such as the very successful Young Women in Computing (YWiC) summer camps.  
+Rebecca is also the state lead for the NM Girls Collaborative, and coordinates or is otherwise involved in several other initiatives to empower, celebrate, and acknowledge the work of professional and young women, following her passion to bring together and support women in their chosen fields, through collaboration, leveraging of resources, and community involvement. 
+<br><img src="http://nmnwse.org/Images/r_hand.gif"><img src="http://nmnwse.org/Images/spacer.gif" height=18 width=1>&nbsp;<a href="NMNWSE_2015_elections_Rebecca_Galves_profile.pdf">Click here for Becca's full profile.</a>
+<br><img src="http://nmnwse.org/Images/bl_pin.gif"><I> Southern Chapter Member, Las Cruces and NMNWSE State EYH Chairs</I>
+
+<! ------------------------------------------------------------ >
+
+<! Vice Pres >
+<P>
+<center>
+<hr width="30%">
+<B><FONT color="#cb6940" size="+2">VICE-PRESIDENT / PRESIDENT ELECT</b></font>
+</center>
+<P>
+<img src="http://nmnwse.org/Images/button.gif">&nbsp;<font size="+1"><B>NEED VOLUNTEER</b></font> 
+<br>We currently have no volunteer to run for this position - please contact the NMNWSE Board if you might be willing.
+<!--
+  <img src="http://nmnwse.org/Images/button.gif">&nbsp;<font size="+1"><B>Sarah Vance</b></font> 
+<br>
+Sarah Vance grew up in Santa Fe and went to college at Iowa State University where she received a BS in Chemical Engineering.  She began her professional career as an Environmental Engineer at Cambrex Charles City, a pharmaceutical manufacturer in Iowa. She then moved to Pittsburgh to work for Montauk Energy, a green energy company.  She's recently returned to Albuquerque to work for the GCC Rio Grande cement plant in Tijeras. Sarah is passionate about young women pursuing engineering careers in New Mexico.  She realizes that our young women need role models and an awareness of what the career can offer and would like to be Vice-President of the New Mexico Network for Women in Science and Engineering to continue their mission along these goals.
+<br><img src="http://nmnwse.org/Images/bl_pin.gif"><I> Central Chapter Member.</I>
+-->
+
+<! ------------------------------------------------------------ >
+<! Treasurer (Secretary runs next yr) >
+<P>
+<center>
+<hr width="30%">
+<B><FONT color="#cb6940" size="+2">TREASURER</b></font>
+</center>
+
+<P>
+<img src="http://nmnwse.org/Images/button.gif">&nbsp;<font size="+1"><B>Jeanne Banks</b></font>
+<br>
+<! 2014 profile >
+Jeanne Banks has been a member of the New Mexico Network for Women in Science and Engineering for over 30 years, having served as State President and Vice President, State EYH Coordinator and has chaired the Annual Meeting and Policies and Procedures Committees.  She has supported the Central EYH in several capacities over the years, most recently as the Workshop Presenter Coordinator.  She has served as Treasurer in other organizations for a total of 12 years.  Her other involvement in support of women's education is the American Business Women's Association.  She has been an ABWA member for almost 35 years and served as an officer at the local, regional and national levels.
+<P>
+Jeanne retired from Northrop Grumman after a 34+ year career as a program manager, system developer and test director.  In addition to her organizational involvement, she now spends her time hiking with her husband, John, traveling, and spoiling her grandchildren.  
+
+<br><img src="http://nmnwse.org/Images/bl_pin.gif"><I> Current NMNWSE Treasurer and NMNWSE Central Chapter President. Member since 1986.</I>
+
+
+<! ------------------------------------------------------------ >
+<! at Large >
+<P>
+<center>
+<hr width="30%">
+<B><FONT color="#cb6940" size="+2">AT-LARGE</b></font>
+</center>
+
+<P>
+<img src="http://nmnwse.org/Images/button.gif">&nbsp;<font size="+1"><B>Sarah Balizan</b></font> <br>
+Sarah Balizan is a Las Cruces native who attended NMSU for a bachelors of Biology/Biochemistry and a Masters of Science in Biology and Biotechnology. She worked on research and development in genetics research from squid to algae, before changing careers to education. She has been at Dona Ana Community College (DACC) for 6 yrs teaching Anatomy & Physiology 2 and Cellular and Organismal Biology. She oversees and developed Anatomy Academy for grade school education and outreach of anatomy through current DACC students teaching 5th graders. She is also co-chair of the DACC STEM student group. She loves teaching along with running and outdoor adventures. Fun tidbit, she has a rescue mix (kind way of saying mutt) who is named Liger. Liger believes she is a 75lb lapdog and will be taking her therapy dog test in December.
+<br><img src="http://nmnwse.org/Images/bl_pin.gif"><I> Southern Chapter Member.</I>
+
+
+<! ================================================== >
+<! continuing and departing >
+
+<P> &nbsp;<br>
+<center>
+<hr width="30%">
+</center>
+<B><FONT color="#cb6940" size="+1">Continuing &amp; Departing Board Members</b></font>
+
+<! Oct 2016 elections - continuing and departing >
+<P>
+<img src="http://nmnwse.org/Images/gr_dot.gif" height=10>
+<B>Continuing Executive Board members:</b>
+<br>&nbsp;&nbsp;&nbsp; Rebecca Galves, Southern Chapter, changing from President to Past-President, running for Pres
+<br>&nbsp;&nbsp;&nbsp; Barbara Torres, Central Chapter, continuing as Secretary
+<!--
+Continuing chapter reps - not elected statewide so don't list
+<br>&nbsp;&nbsp;&nbsp; Jan Frigo, Northern Chapter, continuing as Nothern Chapter President, Santa Fe-spring EYH chair
+<br>&nbsp;&nbsp;&nbsp; Becca Galves, Southern Chapter, continuing as Southern Chapter President, Las Cruces and state EYH chair
+-->
+
+<P><img src="http://nmnwse.org/Images/gr_dot.gif" height=10>
+<b>Continuing At-Large Board Members:</b>
+<br>&nbsp;&nbsp;&nbsp; Diane Albert, Central Chapter
+<br>&nbsp;&nbsp;&nbsp; Claudia Barreto, Central Chapter
+<br>&nbsp;&nbsp;&nbsp; Cheri Burch, Central Chapter
+<br>&nbsp;&nbsp;&nbsp; Lina Germann, Central Chapter
+<br>&nbsp;&nbsp;&nbsp; Dana Roberson, Central Chapter
+<br>&nbsp;&nbsp;&nbsp; Rajaa Shindi, Central Chapter
+
+<P><img src="http://nmnwse.org/Images/gr_dot.gif" height=10>
+<b>Departing Board members running for re-election:</b>
+
+<P><img src="http://nmnwse.org/Images/gr_dot.gif" height=10>
+<b>Departing Board members not running for re-election:</b>
+<br>&nbsp;&nbsp;&nbsp; Tinka Gammel, Central Chapter
+<br>&nbsp;&nbsp;&nbsp; Karmen Lappo, Central Chapter
+
+
+<! ------------------------------------------------------------------------ >
+
+<! bottom stuff >
+
+<! ================================================== >
+
+<P>Please visit the <a href="http://nmnwse.org/Board.php">board webpage</a> for current <b>Chapter Presidents, EYH Chairs, and Ad-hoc and Standing Committee Chairs</b>, who are not elected by NMNWSE statewide Elections and are presumed to not be changing unless they have notified the board.
+<P><img src="http://nmnwse.org/Images/bl_pin.gif"> <B>And a big THANKS! to all our current and past board members and EYH and committee chairs!</b>
+
+
+<! --------------- >
+
+<?php
+include("../../../Includes/pagebot_nmnwse.php");
+?>
