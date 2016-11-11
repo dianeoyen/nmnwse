e3ca4eb1ce988a7f7a67941917a9cf41ddfddbd6
dianeoyen <doyen@cs.unm.edu>
2016-11-11 15:32:28 -0700

    Archiving old CEYH info.

--- htdocs/ceyh/index.php
+++ htdocs/ceyh/index.php
@@ -1,177 +1,93 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-  <title>NMNWSE Central Chapter Expanding Your Horizons (EYH) Conference</title>
+  <meta charset="UTF-8">
+  <title>NMNWSE Central Expanding Your Horizons (EYH) Conference</title>
+</head>
 
-<?php
-include("Includes/pagetop1_ceyh_p1.php");
-?>
-2016 NMNWSE Central Chapter 
-<?php
-if($_GET['printable'] != 1){
-  echo " ";
-}
-else{
-  echo " <br> ";
-}
-?>
-Expanding Your Horizons<sup>tm</sup> (EYH)
-<?php
-include("Includes/pagetop2_ceyh_2011.php");
-?>
+<body>
+  <header>
+    <?php include("Includes/pagetop1_ceyh_p1.php"); ?>
+    NMNWSE Central
+    <?php
+       if($_GET['printable'] != 1){
+       echo " ";
+       }
+       else{
+       echo " <br> ";
+       }
+    ?>
+    Expanding Your Horizons<sup>tm</sup> (EYH)
+    <?php
+       include("Includes/pagetop2_ceyh_2011.php");
+    ?>
+  </header>
 
-<! ---------- main content -------- >
+  <main>
+    <font face="Arial,Helvetica">
+      <P>
+	<table border=0 cellspacing=0 cellpadding=0><tr><td valign="center">
+	      <font size="+2"><b>
+		  <font color="#4b9fc3">
+		    <! font color="#cb6940">
+		    January 2017
+		    <br> University of New Mexico (UNM), Main Campus
+		    <br> grades 5-9
+	      </font></b></font>
+	    </td><td>
+	</td></tr></table>
+      </P>
 
+      <P> <! Registration to open in early January >
+	Planning for the 2017 EYH has begun.
+      </P>
 
-<! -------- Albuquerque EYH ---------- >
-<P>
-
-
-<font face="Arial,Helvetica">
-<P>
-<table border=0 cellspacing=0 cellpadding=0><tr><td valign="center">
-<font size="+2"><b>
- <font color="#4b9fc3">
- <! font color="#cb6940">
-  <! Saturday, Jan 26, 2013, 8:30am-12noon >
-  <! Saturday, Jan 25, 2014, 8:30am-12noon >
-  <! Saturday, Feb 7, 2015, 8:30am-12noon >
-  Saturday, January 30, 2016 &nbsp; <! img src="http://nmnwse.org/Images/updated.gif" >
-  <br> University of New Mexico (UNM), Main Campus
-  <br> grades 5-9
-</font></b></font>
-</td><td>
-</td></tr></table>
-</P>
-
-<P><img src="http://nmnwse.org/Images/updated.gif">&nbsp;1/1/16: Registration opens Jan 4
-<! 11/9/15: Planning for the 2016 EYH has begun, and the date tentatively set for Jan 30, 2016. >
-
-<!--
-<P><img src="http://nmnwse.org/Images/updated.gif">&nbsp;11/24/14:&nbsp;<img src="http://nmnwse.org/Images/r_hand.gif">&nbsp;<a href="http://nmnwse.org/ceyh/Flyers/CEYH_2015_FLYER.pdf">Postable 2015 Flyer</a>
-<P><img src="http://nmnwse.org/Images/updated.gif"> 3/13/14: Planning for the 2015 EYH has begun, and the date tentatively set for Feb 7, 2015.
--->
-<P>
-If you'd like to be involved in the 2016 Albuquerque EYH planning, please use the <a href="http://nmnwse.org/ceyh/Forms/volunteer_form.php">volunteer form</a>, or email the planning committee at <a href="mailto:Central_EYH-L@list.unm.edu?Subject=CEYH interest">Central_EYH-L@list.unm.edu</a>.  
-<b>Volunteers are needed for several committee positions!</b>
-<br><img src="http://nmnwse.org/Images/bl_pin.gif">&nbsp;<a href="http://nmnwse.org/ceyh/dayofvolunteer">2016 CEYH "day-of volunteer" sign up form</a>
-
-<P><img src="http://nmnwse.org/Images/bl_pin.gif">&nbsp;<a href="http://nmnwse.org/ceyh/Flyers/CEYH_reg_opens_20160104.pdf">2016 CEYH "Registration Opens Jan 4, 2016" flyer</a>
+      <!--
+	  <P><img src="http://nmnwse.org/Images/updated.gif">&nbsp;11/24/14:&nbsp;<img src="http://nmnwse.org/Images/r_hand.gif">&nbsp;<a href="http://nmnwse.org/ceyh/Flyers/CEYH_2015_FLYER.pdf">Postable 2015 Flyer</a>
+	  <P><img src="http://nmnwse.org/Images/updated.gif"> 3/13/14: Planning for the 2015 EYH has begun, and the date tentatively set for Feb 7, 2015.
+	-->
+      <!--
+	  <br><img src="http://nmnwse.org/Images/bl_pin.gif">&nbsp;<a href="http://nmnwse.org/ceyh/dayofvolunteer">2016 CEYH "day-of volunteer" sign up form</a>
+	  
+          <P><img src="http://nmnwse.org/Images/bl_pin.gif">&nbsp;<a href="http://nmnwse.org/ceyh/Flyers/CEYH_reg_opens_20160104.pdf">2016 CEYH "Registration Opens Jan 4, 2016" flyer</a>
 <P><img src="http://nmnwse.org/Images/bl_pin.gif">&nbsp;<a href="http://nmnwse.org/ceyh/Flyers/CEYH_SaveTheDate_2016.pdf">2016 CEYH "Save the Date" flyer</a>
+         -->
+
+      <P>
+	<B>If you would like to volunteer to help with the conference:</b>
+	<!--&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/gr_dot.gif" height=10>-->
+	<ul>
+	  <li>contact the CEYH planning committee at <a href="mailto:CENTRAL_EYH-L@list.unm.edu">CENTRAL_EYH-L@list.unm.edu</a>, </li>
+	  <li>fill in the <a href="http://nmnwse.org/ceyh/Forms/volunteer_form.php">online volunteer form</a>, or </li>
+	  <li>see the <a href="http://nmnwse.org/ceyh/volunteer.php">info for volunteers page</a> for planning information, including instructions on how to subscribe to the listserver. </li>
+	</ul>
+      </P>
+
+      <P>
+	<b>For EYH registration information,</b> please check back around January 1st, 2017.
+      </P>
+
+      <hr>
 
 
-<P>For EYH 2016 registration information, please check back around 1 Jan 2016.
-<br><img src="http://nmnwse.org/Images/button.gif">&nbsp;<b>Information below is for the 2015 conference</b>, held February 7, 2015. The 2016 conference will be similar.
-<hr>
+      <P align="center">
+	<img src="http://nmnwse.org/ceyh/Images/ceyh_jazz.jpg" width=70%>
+      </P>
 
+      <p align="center">
+	<a href="archive.php">View photos of previous conferences</a>
+      </p>
 
-<!--
-<P><img src="http://nmnwse.org/Images/button.gif"> 1/27/2014: The 2014 EYH is over - and it was a huge success.  We had about 230 students who left turned-on, jazzed-up, and excited about math and science. <B>Thanks to all who helped make it a success!</b>
+      <p>
+	Here is your chance to meet with female professionals from the community who have found satisfying careers in law, mathematics, engineering, computing, medicine, and other non-traditional technical careers. Each student attends three workshops, led by a woman professional who shares an aspect of her technical field. Workshops typically have 10 to 15 students; which facilitate interaction between the presenter and students. Presenters also share personal experiences and other influences that have impacted their decisions as they have developed their careers.
+      </p>
 
-<P><img src="http://nmnwse.org/Images/updated.gif">  2014 Conference was move from Feb 1 to Jan 25 to avoid conflict with Science Olympiad at UNM on February 1.
-<P><img src="http://nmnwse.org/Images/updated.gif"> 12/30/2013: Registration now open!
--->
+  </main>
 
+  <footer>
+    <?php include("Includes/pagebot_ceyh.php"); ?>
+  </footer>
 
-<P><img src="http://nmnwse.org/Images/button.gif">
-<a href="http://www.expandingyourhorizons.org/conferences/Albuquerque/"> Conference information for attendees</a>
-<br>Note the EYH registration pages have not yet been updated for 2016 - please check back around 1 Jan 2016. 
-<br>
-<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/bl_pin.gif"><a href="http://nmnwse.org/ceyh/Flyers/CEYH_2015_FLYER.pdf"> 2015 Conference flyer suitable for posting</a>
-<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/bl_pin.gif"><a href="http://www.eyhreg.org/student-registration.php?cid=13412&lang=EN">student registration form</a></b>
-<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/bl_pin.gif"><a href="http://www.eyhreg.org/adult-registration.php?cid=13412&lang=EN">adult registration form</a></b>
-<!--
-<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/bl_pin.gif"><a href="http://nmnwse.org/ceyh/direc.php"> Directions to conference</a>
-<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/bl_pin.gif"><a href="http://nmnwse.org/ceyh/CEYH2013_student_sched_map.pdf">student schedule + campus map</a></b>
-<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/bl_pin.gif"><a href="http://nmnwse.org/ceyh/Forms/release_form.pdf">student release form</a></b><! * > (bring signed hardcopy to CEYH) 
-<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/gr_dot.gif" height=10><font size="-1"><a href="http://nmnwse.org/ceyh/Forms/release_form.htm">(if you have trouble with the above release, click here for an html version)</a></font></b>
-<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/bl_pin.gif">Parents should plan to pick up their children by 12:15pm.
--->
-
-
-<P><img src="http://nmnwse.org/Images/button.gif">
-The 2015 Albuquerque EYH targets girls in grades 5-9. Boys in these grades are also welcome to attend, and there will be a program for teachers and other adults who accompany the students to EYH.
-<P><img src="http://nmnwse.org/Images/button.gif">
-All EYH conferences feature hands-on workshops designed to encourage girls to enter STEM Careers. See <a href="http://nmnwse.org/eyh/eyh_whatis.php">the NMNWSE EYH pages</a> for more information on NMNWSE EYH conferences.
-<P><img src="http://nmnwse.org/Images/button.gif">
-If you have questions, please contact us at <a href="mailto:Central_EYH-L@list.unm.edu?Subject=CEYH interest">Central_EYH-L@list.unm.edu</a>, or via the NMNWSE webmaster Tinka Gammel, <a href="mailto:webmaster@nmnwse.org?Subject=CEYH">webmaster@nmnwse.org</a>.
-
-</font>
-
-<!--
-<P><font size="-1">* <B><I>Note on pdf forms:</I></b>
-if you fill in the form electronically and have trouble saving information entered, save to your desktop before filling in, then open the copy on your desktop with Adobe Reader or similar Acrobat product. If this does not fix the problem, please contact the <a href="mailto:webmaster@nmnwse.org">webmaster</a>, or just print the blank form and fill in by hand.</font>
--->
-
-<P>
-<hr><P>
-<img src="http://nmnwse.org/Images/r_hand.gif">
-<B>If you would like to volunteer to help with the conference:</b>
-<br>
-&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/gr_dot.gif" height=10>
-contact the CEYH planning committee at <a href="mailto:CENTRAL_EYH-L@list.unm.edu">CENTRAL_EYH-L@list.unm.edu</a>, 
-<br> 
-&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/gr_dot.gif" height=10>
-fill in the <a href="http://nmnwse.org/ceyh/Forms/volunteer_form.php">online volunteer form</a>, or
-<br> 
-&nbsp;&nbsp;&nbsp;<img src="http://nmnwse.org/Images/gr_dot.gif" height=10>
-see the <a href="http://nmnwse.org/ceyh/volunteer.php">info for volunteers page</a> for planning information, including instructions on how to subscribe to the listserver.
-
-<P> <hr>
-
-<!--
-<P>
-CEYH 2013 will be similar to 
-CEYH 2012, which was held on Saturday, January 28, 2012 at UNM.
-<br>
-<B>&star; Info below for 2012. 2013 updates expected Dec 2012. &star;</B>
--->
-
-<!--
-<P>
-<table cellspacing=0 cellpadding=5><tr><td valign="center"><img src="http://nmnwse.org/Images/new2.gif"></td>
-   <td valign="top">
-     <a href="http://twitter.com/CentralEYH"><b>Follow us on Twitter!</b></a>
-     <br>
-     <a href="http://www.facebook.com/home.php?#/group.php?gid=188269968818&ref=ts"><b>Visit <img src="http://nmnwse.org/Images/spacer.gif" width=1 height=16>us on Facebook!</b></a>
-  </td>   </tr>
-</table>
--->
-
-
-
-<P align="center">
-&nbsp;<br>
-<img src="http://nmnwse.org/ceyh/Images/ceyh_jazz.jpg" width=70%>
-</P>
-
-<p align="center"><a href="archive.php">View photos of previous conferences</a>
-</p>
-
-<p>
-Here is your chance to meet with female professionals from the community who have found satisfying careers in law, mathematics, engineering, computing, medicine, and other non-traditional technical careers. Each student attends three workshops, led by a woman professional who shares an aspect of her technical field. Workshops typically have 10 to 15 students; which facilitate interaction between the presenter and students. Presenters also share personal experiences and other influences that have impacted their decisions as they have developed their careers.
-</p>
-
-<!--
-<p>
-<table width="80%"><tr><td>&quot;<em>I am so glad to find links to this great program after so many years.
-  I give a lot of credit to this program for my success in the engineering field.
-  I went through this program of course at UNM while in 7th grade at Polk
-   Middle School. I decided then at
-  the conference that I wanted to be an engineer. I graduated from UNM with a
-  BSEE in Dec 1997 and I am thankful for this program. I have moved around the
-  country and now reside in the Houston area. Please pat yourself on the back for contributing to such a successful
-  program. Thank you. I hope to locate a similar program in the area so I can
-  volunteer my time.&quot;</em> - Tammy </p>
-</td></tr></table>
--->
-
-
-<br> &nbsp;
-<! ---------- end main content -------- >
-
-<?php
-include("Includes/pagebot_ceyh.php");
-?>
-
-
-
+</body>
+</html>
