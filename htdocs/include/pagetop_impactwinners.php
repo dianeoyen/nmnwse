26b19bd13da122982fdd29020da3258eab44b70a
dianeoyen <doyen@cs.unm.edu>
2016-12-02 21:02:16 -0700

    Added images and pagetops for Impact Award and Impact Award Winners.

---
+++ htdocs/include/pagetop_impactwinners.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<! IMPACT Award Winners top bar >
+
+<img src="http://nmnwse.org/Images/linefrill.gif" alt="" class="decoration" height="7" width="100%">
+
+<div style="float:left" width="150">
+  <img src="http://nmnwse.org/Images/CSWLogo30.jpg" alt="NM Commission on the Status of Women logo" border="0" class="decoration" vspace="3" width="100%">
+  <br>NM Commission on<br> the Status of Women
+</div>
+  
+<a href="http://nmnwse.org/" style="float:right">
+  <img src="http://nmnwse.org/Images/NMNWSE4a.gif" alt="NMNWSE logo" class="decoration" vspace="3" width="150" border="0"></a>
+
+<h1>
+<?php
+   if($_GET['printable'] != 1){
+   echo "<img src=\"http://nmnwse.org/Images/ImpactWinnersImg.jpg\" width=500>";
+   }
+   else{
+   echo "IMPACT! Award Winners";
+   }
+   ?>
+</h1>
+
+<img alt="" src="http://nmnwse.org/Images/linefrill.gif" class="decoration" height="7" vspace="3" width="100%">
+
+
+
+
