f0dd7ab529c7218734c075d2a9fb2391add6908b
dianeoyen <doyen@cs.unm.edu>
2017-03-30 22:40:26 -0600

    Reformatted newsletter page.

--- htdocs/News.php
+++ htdocs/News.php
@@ -1,111 +1,87 @@
-<html>
-<head>
-<title>NMNWSE Newsletters</title>
-<?php
-include("Includes/pagetop1_nmnwse.php");
-?>
-NEWSLETTERS
-<?php
-include("Includes/pagetop2_nmnwse.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <title>NMNWSE Newsletters</title>
+    <link rel="stylesheet" href="css/nmnwse.css">
+  </head>
 
-&nbsp;<br>
-<p>
-The Network News is distributed regularly to current members of the network. 
+  <body>
+    <header>
+      <?php
+        $banner_title = "NMNWSE Newsletters";
+        include("include/pagetop_nmnwse.php");
+        ?>
+    </header>
+    
+    <main>
 
-<P>
-To submit items to the newsletter or request copies of the newsletter contact
-the NMNWSE webmaster at <a href="mailto:webmaster@nmnwse.org">webmaster@nmnwse.org</a>,
-or contact the NMNWSE board at  
-<a href="mailto:NMNWSE_Board-l@list.unm.edu">NMNWSE_Board-l@list.unm.edu</a>
+      <! NAV >
+      <?php
+        include("include/sidebar.php");
+        ?>
 
-<P>
-<table>
- <tr>
-  <td valign="top" colspan=3>
-   <font size=+1><font color="#cb6940"><b> Current Newsletter: </b></font></font>
-  </td>
- </tr>
- <tr>
-  <td valign="top" colspan=3>
-   <ul>
-     <li> <a href="News/NMNWSE_newsletter_201508.pdf">August 2015</a> </li>
-   </ul>
-  </td>
- </tr>
- <tr>
-  <td valign="top" colspan=3>
-   <font size=+1><font color="#cb6940"><b> Past Newsletters: </b></font></font>
-  </td>
- </tr>
- <tr>
-  <td valign="top">
-   <ul>
-     <li> <a href="News/NMNWSE_newsletter_201506.pdf">June 2015</a> </li>
-     <li> <a href="News/NMNWSE_newsletter_09_2014.pdf">September 2014</a> </li>
-     <li> <a href="News/NMNWSE_newsletter_08_2013.pdf">August 2013</a> </li>
-     <li> <a href="News/NMNWSE_PresNote_Spring2013.pdf">Spring 2013</a><sup>a</sup> </li>
-     <li> <a href="News/NMNWSE_newsletter_08_2012.pdf">October 2012</a> </li>
-     <li> <a href="News/NMNWSE_newsletter_09_2010.pdf">September 2010</a> </li>
-     <li> <a href="News/NMNWSE_newsletter_08_2009.pdf">August 2009</a> </li>
-     <li> <a href="News/NMNWSE_newsletter_09_2008.pdf">September 2008</a> </li>
-     <li> <a href="News/NMNWSE_newsletter_06_2008.pdf">June 2008</a> </li>
-     <li> <a href="News/NMNWSE_newsletter_09_2007.pdf">September 2007</a> </li>
-     <li> <a href="News/NMNWSE_newsletter_04_2007.pdf">April 2007</a> </li>
-     <li> <a href="News/NMNWSE_newsletter_01_2007.pdf">January 2007</a> </li>
-     <li> <a href="News/NMNWSE_May_2006.pdf">May/June 2006</a> </li>
-     <li> <a href="News/200312.html">December 2003</a> </li>
-   </ul>
-  </td>
-  <td valign="top">
-   <ul>
-     <li> <a href="News/200305.html">May 2003</a> </li>
-     <li> <a href="News/200202.html">February 2002</a> </li>
-     <li> <a href="News/200108.pdf">August 2001</a> </li>
-     <li> <a href="News/200103.html">March 2001</a> </li>
-     <li> <a href="News/200101.html">January 2001</a> </li>
-     <li> <a href="News/200011.html">November 2000</a> </li>
-     <li> <a href="News/200007.html">July 2000</a> </li>
-     <li> <a href="News/200005.html">May 2000</a> </li>
-     <li> <a href="News/200003.html">March 2000</a> </li>
-     <li> <a href="News/200001.html">January 2000</a> </li>
-     <li> <a href="News/9911.html">November 1999</a> </li>
-     <li> <a href="News/9906.html">June 1999</a> </li>
-     <li> <a href="News/9904.html">April 1999</a> </li>
-   </ul>
-  </td>
-  <td valign="top">
-   <ul>
-     <li> <a href="News/9901.html">January 1999</a> </li>
-     <li> <a href="News/9808.html">August 1998</a> </li>
-     <li> <a href="News/9806.html">June 1998</a> </li>
-     <li> <a href="News/9802.html">February 1998</a> </li>
-     <li> <a href="News/9709.html">October 1997</a> </li>
-     <li> <a href="News/9707.html">July 1997</a> </li>
-     <li> <a href="News/9702.html">February 1997</a> </li>
-     <li> <a href="News/9611.html">November 1996</a> </li>
-     <li> <a href="News/9607.html">July 1996</a> </li>
-     <li> <a href="News/9604.html">April 1996</a> </li>
-     <li> <a href="News/9601.html">January 1996</a> </li>
-     <li> <a href="News/9510.html">October 1995</a> </li>
-     <li> <a href="News/9507.html">July 1995</a> </li>
-   </ul>
-  </td>
- </tr>
- <tr>
-  <td colspan=3>
-     <sup>a</sup>Mini-Newsletter/Note from President
-  </td>
- </tr>
-</table>
+      <h2> Newsletter Archive </h2>
 
-<!--
-<P>
-<i>Updated Sep 2007</i> 
--->
+      <div class="multicolumn">
+        <ul>
+          <li> <a href="News/NMNWSE_newsletter_201508.pdf">August 2015</a> </li>
+          <li> <a href="News/NMNWSE_newsletter_201506.pdf">June 2015</a> </li>
+          <li> <a href="News/NMNWSE_newsletter_09_2014.pdf">September 2014</a> </li>
+          <li> <a href="News/NMNWSE_newsletter_08_2013.pdf">August 2013</a> </li>
+          <li> <a href="News/NMNWSE_PresNote_Spring2013.pdf">Spring 2013</a><sup>a</sup> </li>
+          <li> <a href="News/NMNWSE_newsletter_08_2012.pdf">October 2012</a> </li>
+          <li> <a href="News/NMNWSE_newsletter_09_2010.pdf">September 2010</a> </li>
+          <li> <a href="News/NMNWSE_newsletter_08_2009.pdf">August 2009</a> </li>
+          <li> <a href="News/NMNWSE_newsletter_09_2008.pdf">September 2008</a> </li>
+          <li> <a href="News/NMNWSE_newsletter_06_2008.pdf">June 2008</a> </li>
+          <li> <a href="News/NMNWSE_newsletter_09_2007.pdf">September 2007</a> </li>
+          <li> <a href="News/NMNWSE_newsletter_04_2007.pdf">April 2007</a> </li>
+          <li> <a href="News/NMNWSE_newsletter_01_2007.pdf">January 2007</a> </li>
+        <li> <a href="News/NMNWSE_May_2006.pdf">May/June 2006</a> </li>
+        <li> <a href="News/200312.html">December 2003</a> </li>
+        <li> <a href="News/200305.html">May 2003</a> </li>
+        <li> <a href="News/200202.html">February 2002</a> </li>
+        <li> <a href="News/200108.pdf">August 2001</a> </li>
+        <li> <a href="News/200103.html">March 2001</a> </li>
+        <li> <a href="News/200101.html">January 2001</a> </li>
+        <li> <a href="News/200011.html">November 2000</a> </li>
+        <li> <a href="News/200007.html">July 2000</a> </li>
+        <li> <a href="News/200005.html">May 2000</a> </li>
+        <li> <a href="News/200003.html">March 2000</a> </li>
+        <li> <a href="News/200001.html">January 2000</a> </li>
+        <li> <a href="News/9911.html">November 1999</a> </li>
+        <li> <a href="News/9906.html">June 1999</a> </li>
+        <li> <a href="News/9904.html">April 1999</a> </li>
+        <li> <a href="News/9901.html">January 1999</a> </li>
+        <li> <a href="News/9808.html">August 1998</a> </li>
+        <li> <a href="News/9806.html">June 1998</a> </li>
+        <li> <a href="News/9802.html">February 1998</a> </li>
+        <li> <a href="News/9709.html">October 1997</a> </li>
+        <li> <a href="News/9707.html">July 1997</a> </li>
+        <li> <a href="News/9702.html">February 1997</a> </li>
+        <li> <a href="News/9611.html">November 1996</a> </li>
+        <li> <a href="News/9607.html">July 1996</a> </li>
+        <li> <a href="News/9604.html">April 1996</a> </li>
+        <li> <a href="News/9601.html">January 1996</a> </li>
+        <li> <a href="News/9510.html">October 1995</a> </li>
+        <li> <a href="News/9507.html">July 1995</a> </li>
+      </ul>
 
-<P>&nbsp;
-<?php
-include("Includes/pagebot_nmnwse.php");
-?>
+      <p>
+        <sup>a</sup>Mini-Newsletter/Note from President
+      </p>
+
+    </main>
+
+    <!-- footer -->
+    <footer>
+      <?php
+        include("includes/pagefoot_nmnwse.php");
+        ?>
+    </footer>
+
+  </body>
+</html>
 
