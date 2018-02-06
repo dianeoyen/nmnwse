19c424b0c6738434eace58aad6115a1fb4f44cc6
Diane Oyen <doyen@lanl.gov>
2018-02-05 21:56:07 -0700

    updates to membership page, and re-formatting

---
+++ htdocs/join/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <title>NMNWSE Membership</title>
+    <link rel="stylesheet" href="../css/nmnwse.css">
+  </head>
+
+  <body>
+    <header>
+      <?php
+        $banner_title = "NMNWSE Membership";
+        include("../include/pagetop_nmnwse.php");
+        ?>
+    </header>
+
+    <main>
+
+      <! NAV >
+      <?php
+        include("../include/sidebar.php");
+        ?>
+      
+      <h2> Download info about us </h2>
+      <ul>
+        <li>
+          <a href="http://nmnwse.org/WhoWeAreNMNWSE.pdf">Who We Are trifold Brochure</a>
+        </li>
+        <li>
+          <a href="http://nmnwse.org/Membership/MembershipLetter2016.pdf">Letter from the Membership Chair</a>
+        </li>
+      </ul>
+
+      <h2> Why join NMNWSE? </h2>
+      <p>
+        Membership in the network helps to support all of the efforts
+        of the group. Members attend chapter meetings, receive the printed
+        version of <a href="http://nmnwse.org/News.php">The Network News</a>, sponsor and
+        participate in Expanding Your Horizons Conferences or other
+        outreach and chapter activities, and have the opportunity to
+        attend and make technical presentations at the annual meeting.
+      </p>
+      <p>
+        Members also receive an electronic copy of the current NMNWSE roster, which contains the contact information for all members to facillitate networking. 
+      </p>
+      <p>
+        Professional membership in the New Mexico Network for Women in
+        Science and Engineering is open to New Mexico women working (or
+        interested) in a scientific or engineering environment. In December 2008 the Board
+        voted to changed the membership rates for the first time in many years. The rate is
+        now $40.00 per year for regular members, and the reduced rate for students, teachers, retired members, and non-employed members is $10.00.
+      </p>
+
+      <h2> Join us! </h2>
+      <p>
+        To apply for membership in the New Mexico Network for Women in
+        Science &amp; Engineering, please submit a completed <A HREF="http://nmnwse.org/MemberForm.pdf">Membership
+          Form</A>:
+        <br><table border=0><tr><td align="left">
+              &nbsp;&nbsp;<img src="http://nmnwse.org/Images/bl_pin.gif">&nbsp;<A HREF="http://nmnwse.org/MemberForm.pdf">MemberForm.pdf</A>
+            </td><td align="right">
+              &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;<img src="http://nmnwse.org/Images/bl_pin.gif">&nbsp;<A HREF="http://nmnwse.org/MemberForm.html">MemberForm.html</A>
+            </td><td align="right">
+              &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;<img src="http://nmnwse.org/Images/bl_pin.gif">&nbsp;<A HREF="http://nmnwse.org/MemberForm.doc">MemberForm.doc</A>
+        </td></tr></table>
+        <br>and a check for the appropriate amount (above)
+        to:
+      </p>
+
+      <p>
+   
+        New Mexico Network for Women in Science &amp; Engineering<BR>
+        PO Box 51926<BR>
+        Albuquerque, New Mexico, 87181  USA
+      </p>
+
+      <p>
+        <ul>
+          <li>
+            Membership fees are not tax deductible.
+          </li>
+          <li>
+            There is space to include a tax-deductible donation in addition to your membership fees on the membership form.
+          </li>
+          <li>
+            <a href="http://nmnwse.org/donate.php">Click here</a> for more information about donating to NMNWSE.
+          </li>
+        </ul>
+      </p>
+   
+      <p class="note">
+        Notes about the forms
+        <ul>
+          <li>
+            The html membership form is not a web form which can be filled in online.
+          </li>
+          <li>
+            The pdf version is a fillable form - please <a href="mailto:webmaster@nmnwse.org">contact the webmaster</a> if you encounter problems with the form.
+          </li>
+          <li>
+            Problems with the pdf form are often resolved by saving the form to your desktop, filling it in making sure to use an Adobe product to open, saving, then emailing as an attachment to the <a href="mailto:webmaster@nmnwse.org">webmaster</a>.
+          </li>
+          <li>
+            If all else fails, you can simply print, fill in by hand, then scan and email or mail a hardcopy.
+          </li>
+        </ul>
+      </p>
+      
+      <h2> Questions? </h2>
+      <p>
+        For questions about membership, please <a href="mailto:NMNWSE_Board-L@list.unm.edu">contact the board</a>.
+      </p>
+
+    </main>
+
+    <!-- footer -->
+    <footer>
+      <?php
+        include("../include/pagefoot_nmnwse.php");
+        ?>
+    </footer>
+
+  </body>
+</html>
+
