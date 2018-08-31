d661194215d76d08b06d9612a7da5308c26bbe49
Diane Oyen <doyen@lanl.gov>
2018-08-31 11:08:20 -0600

    Update to Las Cruces EYH webpage

--- htdocs/lceyh/index.php
+++ htdocs/lceyh/index.php
@@ -1,19 +1,4 @@
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<HTML>
-<HEAD>
-<META HTTP-EQUIV="Refresh" CONTENT="1; URL=http://www.eyhnmsu.webs.com">
-<TITLE>forward to current Las Cruces Expanding Your Horizons (EYH) webpages</TITLE>
-</HEAD>
-<BODY BGCOLOR="white">
- 
-The current Las Cruces Expanding Your Horizons (EYH) webpages are at:
-
-<P>
-<font size=+1 font="Arial,Helvetica"><B><a href="http://www.eyhnmsu.webs.com">http://www.eyhnmsu.webs.com</a></B></font>
-
-<P>
-You are being automatically forwarded to this address, or click on the above link to go there manually.
-
-</BODY>
-</HTML>
+<?php
+  header('Location: http://www.expandingyourhorizons.org/conferences/LasCruces', true, 301);
+  exit();
+?>
