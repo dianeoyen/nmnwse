62365972dbc0ba7ca3650b3341333bdd6602519f
Diane Oyen <doyen@lanl.gov>
2019-02-19 16:37:17 -0700

    Posted new Policies and Procedures, and reorganized documents

---
+++ htdocs/docs/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <title>NMNWSE Documents </title>
+    <link rel="stylesheet" href="../css/nmnwse.css">
+  </head>
+
+  <body>
+    
+    <header>
+      <?php
+	 $banner_title = "NMNWSE Executive Board Documents";
+	 include("../include/pagetop_nmnwse.php");
+	 ?>
+    </header>
+
+    <main>
+      <?php
+	 include("../include/sidebar.php");
+	 ?>
+      
+      <h2>Board-related Information</h2>
+      <ul>
+	<li>
+	  <a href="http://nmnwse.org/BoardMeetings">Board Meetings</a>
+	</li>
+	<li>
+	  <a href="http://nmnwse.org/docs/NMNWSE_Policies_and_Procedures_2018.pdf">Policies and Procedures</a> (includes duties and responsibilities for board positions)
+	</li>
+	<li>
+	  <a href="http://nmnwse.org/docs/NMNWSE_Bylaws.pdf">Bylaws</a>
+	</li>
+	<li>
+	  <a href="http://nmnwse.org/docs/NMNWSE_ArtInc_orig_retyped.pdf">Articles of Incorporation</a> (as amended 17 Oct 2009; please contact the board if you need a copy showing receipt by the PRC)
+	</li>
+	<li>
+	  <a href="http://nmnwse.org/docs/NMNWSE_Dark_Sky_Resolution.pdf">Dark Sky Resolution</a>
+	</li>
+      </ul>
+
+      <h2>Archive of Documents</h2>
+      <ul>
+        <li>
+	  <a href="http://nmnwse.org/docs/NMNWSE_pnp_200812.pdf">Policies and Procedures, 2008 revision</a>          
+        </li>
+        <li>
+          <a href="http://nmnwse.org/docs/NMNWSE_Policies_and_Procedures_2001.pdf">Policies and Procedures, 2001 revision</a>         
+        </li>
+      </ul>
+
+    </main>
+
+    <footer>
+      <?php
+	 include("../include/pagefoot_nmnwse.php");
+	 ?>
+    </footer>
+
+  </body>
+</html>
