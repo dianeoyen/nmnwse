f7feac01ec53f93485dd181e596f62f6dc8a1cfc
Diane Oyen <doyen@lanl.gov>
2019-02-20 11:16:52 -0700

    Updated NonProfitStatus format and links.

--- htdocs/NonProfitStatus.php
+++ htdocs/NonProfitStatus.php
@@ -1,106 +1,108 @@
-<html>
-<head>
-<title>NMNWSE Non-Profit Information</title>
-<?php
-include("Includes/pagetop1_nmnwse.php");
-?>
-Non-Profit Information
-<?php
-include("Includes/pagetop2_nmnwse.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <title>NMNWSE Non-Profit Information</title>
+    <link rel="stylesheet" href="css/nmnwse.css">
+  </head>
 
-<P> &nbsp;<br>
-<B>NMNWSE is a registered 501(c)(3) public charity.</B>
-Our public charity status is 170(b)(1)(A)(vi).
-Donations are deductable to the extent provided by law.
+  <body>
+    <header>
+      <?php
+        $banner_title = "NMNWSE Non-Profit Information";
+        include("include/pagetop_nmnwse.php");
+        ?>
+    </header>
 
-<P>
-Our legal name is <I><B>"New Mexico Network for Women in Science and Engineering, Inc"</b></I>. We are also known as "NMNWSE", and we were formerly known as the "Network for Women in Science and Engineering, Inc". If you need a copy of our most recent determination letter from the IRS, dated May 8, 2013, please <a href="mailto:NMNWSE_Board-L@list.unm.edu ?Subject=NMNWSE nonprofit status">contact the NMNWSE Board</a>. 
+    <main>
 
-<!--
-Our legal name as filed with the NM PRC is <I><B>"New Mexico Network for Women in Science and Engineering, Inc"</b></I>. We are also known as "NMNWSE", and we were formerly known as the "Network for Women in Science and Engineering, Inc". The change from our former to current legal name is reflected in our determination letter from the IRS dated Jan 6, 2010 which unfortunately reflected a typo and is missing the "in" (i.e., <I><B>"New Mexico Network for Women in Science and Engineering, Inc"</b></I>). We are in the process of filing the paperwork with the IRS to fix this.
--->
+      <! NAV >
+      <?php
+        include("include/sidebar.php");
+        ?>
 
-<P>
-Our tax year is Oct 1 - Sep 30 (eg, FY 2010 was 10-1-2010 thru 9-30-2011).
-Members needing nontaxable transaction certificates or other nontaxable ID numbers for allowable purchases should contact the NMNWSE treasurer. 
+      <p>
+        <B>NMNWSE is a registered 501(c)(3) public charity.</B>
+        Our public charity status is 170(b)(1)(A)(vi).
+        Donations are deductable to the extent provided by law.
+      </p>
+      
+      <p>
+        Our legal name is <I><B>"New Mexico Network for Women in Science and Engineering, Inc"</b></I>. We are also known as "NMNWSE", and we were formerly known as the "Network for Women in Science and Engineering, Inc". If you need a copy of our most recent determination letter from the IRS, dated May 8, 2013, please <a href="mailto:NMNWSE_Board-L@list.unm.edu ?Subject=NMNWSE nonprofit status">contact the NMNWSE Board</a>.
+      </p>
 
-<P>
-See also our <a href="NMNWSE_ArtInc_orig_retyped.doc">Articles of Incorporation</a>, <a href="http://nmnwse.org/bl.html">Bylaws</a>, and <a href="http://nmnwse.org/pnp.html">Policies and Procedures</a>.
+      <p>
+        Our tax year is Oct 1 - Sep 30 (eg, FY 2010 was 10-1-2010 thru 9-30-2011).
+        Members needing nontaxable transaction certificates or other nontaxable ID numbers for allowable purchases should contact the NMNWSE treasurer.
+      </p>
 
-If you need further information, you may:
-<UL>
-<LI>Contact the <a href="mailto:NMNWSE_Board-L@list.unm.edu ?Subject=NMNWSE nonprofit status">NMNWSE Board</a></LI>
-<LI>
-Look us up via the <a href="http://www.nmag.gov">NM Attorney General</a> <a href="https://secure.nmag.gov/CharitySearch/">Charity Search</a>.
-</LI>
-<UL><LI>
-<a href="https://secure.nmag.gov/CharitySearch/CharityDetail.aspx?FEIN=85-0283394">direct link to our entry</a>
-<! status as of 3/30/2013: "extension granted", tax year 2011, 10-1-2011 thru 9-30-2012  > 
-</LI></UL>
-<LI>
-Look us up via the <a href="http://www.sos.state.nm.us/Business_Services/default.aspx">NM Office of the Secretary of State, Business Services Division</a>&nbsp;
-<a href="https://portal.sos.state.nm.us/corps/Corplookup/Lookdn.aspx">Corporation Search</a>.
-<br><I>(Note: the NM Corporations Bureau was recently moved to the Office of the Secretary of State, and is no longer found under the <a href="http://www.nmprc.state.nm.us/">NM Public Regulatory Commission</a>)</I>
-</LI>
-<UL><LI>
-<a href="https://portal.sos.state.nm.us/corps/Corplookup/Details.aspx?Nmscc=1013002">direct link to our entry</a>
-<! Nmscc=1013002, in good standing through 2/15/2014 >
-</LI></UL>
-<LI>
-Look us up via <a href="http://www.guidestar.org/">GuideStar</a>.
-</LI>
-<UL><LI>
-<a href="http://www2.guidestar.org/ReportNonProfit.aspx?ein=85-0283394">direct link to our entry</a>
-</LI>
-<UL><LI>
-<a href="https://www.networkforgood.org/donation/MakeDonation.aspx?ORGID2=85-0283394&source=GS">Donate to us via the Network For Good</a>
-</LI></UL>
-<LI>
-Note that by registering with Guidestar (free) you may view additional 
+      <p>
+        See also our <a href="http://nmnwse.org/docs"> documents page </a> for Articles of Incorporation, Bylaws, and Policies and Procedures.
+
+        If you need further information, you may:
+        <UL>
+          <LI>
+            Contact the <a href="mailto:NMNWSE_Board-L@list.unm.edu ?Subject=NMNWSE nonprofit status">NMNWSE Board</a>
+          </LI>
+          <LI>
+            Look us up via the <a href="http://www.nmag.gov">NM Attorney General</a> <a href="https://secure.nmag.gov/CharitySearch/">Charity Search</a>.
+          </LI>
+          <UL>
+            <LI>
+              <a href="https://secure.nmag.gov/CharitySearch/CharityDetail.aspx?FEIN=85-0283394">direct link to our entry</a>
+              <! status as of 3/30/2013: "extension granted", tax year 2011, 10-1-2011 thru 9-30-2012  > 
+            </LI>
+          </UL>
+          <LI>
+            Look us up via <a href="http://www.guidestar.org/">GuideStar</a>.
+          </LI>
+          <UL>
+            <LI>
+              <a href="http://www2.guidestar.org/ReportNonProfit.aspx?ein=85-0283394">direct link to our entry</a>
+            </LI>
+            <UL>
+              <LI>
+                <a href="https://www.networkforgood.org/donation/MakeDonation.aspx?ORGID2=85-0283394&source=GS">Donate to us via the Network For Good</a>
+              </LI>
+            </UL>
+            <LI>
+              Note that by registering with Guidestar (free) you may view additional 
 details about our organization.
-However, to view details such as our determination letter one would need to purchase Guidestar Premium.
-If you need a copy of the determination letter it is far cheaper to simply <a href="mailto:NMNWSE_Board-L@list.unm.edu ?Subject=IRS Determination Letter">contact the board</a> and ask us for a copy.
+              However, to view details such as our determination letter one would need to purchase Guidestar Premium.
+              If you need a copy of the determination letter it is far cheaper to simply <a href="mailto:NMNWSE_Board-L@list.unm.edu ?Subject=IRS Determination Letter">contact the board</a> and ask us for a copy.
 
-<! including our IRS letter of determination. You may also donate to us via Guidestar. >
-</LI></UL>
-<LI>
+              <! including our IRS letter of determination. You may also donate to us via Guidestar. >
+            </LI>
+          </UL>
+          <LI>
+            Look us up using the <a href="http://www.irs.gov">IRS</a>
+            <a href="http://www.irs.gov/app/pub-78/">Charity Search</a>
+          </LI>
+          <UL>
+            <LI>
+              <a href="https://apps.irs.gov/app/eos/displayAll.do?dispatchMethod=displayAllInfo&Id=4675859&ein=850283394&country=US&deductibility=all&dispatchMethod=searchAll&isDescending=false&city=&ein1=85-0283394&postDateFrom=&exemptTypeCode=al&submitName=Search&sortColumn=orgName&totalResults=1&names=&resultsPerPage=25&indexOfFirstRow=0&postDateTo=&state=All+States">direct link to our entry</a>
+              <! vs  http://apps.irs.gov/app/eos/ - select check page >
+              <! filed 990 for tax year 2011, 10-1-2011 thru 9-30-2012, still have name as New Mexico Network for Women Science and Engineering Inc. (missing in)  >
+            </LI>
+          </UL>
+        </UL>
+      </p>
 
-Look us up using the <a href="http://www.irs.gov">IRS</a>
- <a href="http://www.irs.gov/app/pub-78/">Charity Search</a>
-</LI>
-<UL><LI>
-<a href="http://apps.irs.gov/app/eos/pub78Search.do?ein1=85-0283394&dispatchMethod=searchCharities&submitName=Search">direct link to our entry</a>
-<! vs  http://apps.irs.gov/app/eos/ - select check page >
-<! filed 990 for tax year 2011, 10-1-2011 thru 9-30-2012, still have name as New Mexico Network for Women Science and Engineering Inc. (missing in)  >
-</LI><LI>
-To check our listing in the publically available version of the IRS master file:
-<UL>
-<LI>
-go to <a href="http://www.irs.gov">http://www.irs.gov</a>
-</LI><LI>
-click on "Tax Stats" (under "Information about" at the bottom right of this page)
-</LI><LI>
-This takes you to a <a href="http://www.irs.gov/uac/Tax-Stats-2/">"Tax Statistics" page</a> - now click on "Exempt Org. Master File" (under Charitable & Exempt Org. Statistics at top right)
-</LI><LI>
-This takes you to the page titled <a href="http://www.irs.gov/uac/SOI-Tax-Stats-Exempt-Organizations-Business-Master-File-Extract-(EO-BMF)">"SOI Tax Stats - Exempt Organizations: IRS Master File Data"</a>.<br>
-Scroll down this page to the box for New Mexico and click on the A-Z Excel Table (the person at the IRS recommend not trying to use the unformatted version - she said the formatted was much easier).
-</LI><LI>
-Find us in the Excel file just downloaded under 850283394 or NETWORK FOR WOMEN IN SCIENCE AND ENGINEERING INC
-<br>
-Look at our entry in column N which has the "Foundation Code". It should be "15" 
-</LI>
-</UL>
-The instruction manual for the IRS Master File data (linked from the <a href="http://www.irs.gov/uac/SOI-Tax-Stats-Exempt-Organizations-Business-Master-File-Extract-(EO-BMF)">Master File Data page</a>)
-indicates this code means we are a public charity which receives a substantial part of its support from a governmental unit or the general public, or  170(b)(1)(A)(vi)
-</LI>
-</UL>
-</UL>
-Please note that, while we try to keep this information current, the links to non-NMNWSE sites may have changed since this page was last updated (Aug 2013).
-If the links are broken use your search engine to find the new location, or <a href="mailto:webmaster@nmnwse.org ?Subject=NMNWSE nonprofit page - links broken">contact the NMNWSE webmaster</a>.
+      <p class="note">
+        If the links are broken use your search engine to find the new location, or <a href="mailto:webmaster@nmnwse.org ?Subject=NMNWSE nonprofit page - links broken">contact the NMNWSE webmaster</a>.
+      </p>
 
-<P>&nbsp;
-<?php
-include("Includes/pagebot_nmnwse.php");
-?>
+    </main>
+
+    <! footer >
+    <footer>
+      <?php
+        include("include/pagefoot_nmnwse.php");
+        ?>
+    </footer>
+
+  </body>
+</html>
+
 
